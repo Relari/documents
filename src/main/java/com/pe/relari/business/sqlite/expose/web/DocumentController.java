4158ceb1b6357a90e38bb34533c32b76668f426f
Relari <renzolavador@gmail.com>
2022-02-14 07:17:38 -0500

    feat: The interface for mapping the controller objects is implemented.

--- src/main/java/com/pe/relari/business/sqlite/expose/web/DocumentController.java
+++ src/main/java/com/pe/relari/business/sqlite/expose/web/DocumentController.java
@@ -5,6 +5,8 @@ import com.pe.relari.business.sqlite.documents.model.api.DocumentRequest;
 import com.pe.relari.business.sqlite.documents.model.api.DocumentResponse;
 import com.pe.relari.business.sqlite.documents.model.domain.Document;
 import com.pe.relari.business.sqlite.documents.service.DocumentService;
+import com.pe.relari.business.sqlite.expose.web.mapper.DocumentMapper;
+import com.pe.relari.business.sqlite.expose.web.generate.DocumentsApiDelegate;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
@@ -16,12 +18,13 @@ import org.springframework.stereotype.Component;
 class DocumentController implements DocumentsApiDelegate {
 
   private final DocumentService documentService;
+  private final DocumentMapper documentMapper;
 
   @Override
   public ResponseEntity<List<DocumentResponse>> findAllDocumentsUsingGET() {
     List<DocumentResponse> documentResponses = documentService.findAll()
             .stream()
-            .map(this::mapDocumentResponse)
+            .map(documentMapper::mapDocumentResponse)
             .collect(Collectors.toList());
     return ResponseEntity.ok(documentResponses);
   }
@@ -29,7 +32,7 @@ class DocumentController implements DocumentsApiDelegate {
   @Override
   public ResponseEntity<DocumentResponse> findDocumentByIdUsingGET(Integer id) {
     Document document = documentService.findById(id);
-    DocumentResponse documentResponse = mapDocumentResponse(document);
+    DocumentResponse documentResponse = documentMapper.mapDocumentResponse(document);
     return ResponseEntity.ok(documentResponse);
   }
 
@@ -41,32 +44,11 @@ class DocumentController implements DocumentsApiDelegate {
 
   @Override
   public ResponseEntity<CodeResponse> saveDocumentUsingPOST(DocumentRequest documentRequest) {
-    Document document = mapDocument(documentRequest);
+    Document document = documentMapper.mapDocument(documentRequest);
     String code = documentService.create(document);
 
-    CodeResponse codeResponse = new CodeResponse();
-    codeResponse.documentCode(code);
+    CodeResponse codeResponse = documentMapper.mapCodeResponse(code);
     return ResponseEntity.ok(codeResponse);
   }
 
-  private Document mapDocument(DocumentRequest documentRequest) {
-    return Document.builder()
-            .author(documentRequest.getAuthor())
-            .description(documentRequest.getDescription())
-            .gender(documentRequest.getGender())
-            .numberPages(documentRequest.getNumberPages())
-            .yearPublication(documentRequest.getYearPublication())
-            .build();
-  }
-
-  private DocumentResponse mapDocumentResponse(Document document) {
-    DocumentResponse documentResponse = new DocumentResponse();
-    documentResponse.setAuthor(document.getAuthor())           ;
-    documentResponse.setDescription(document.getDescription());
-    documentResponse.setGender(document.getGender());
-    documentResponse.setNumberPages(document.getNumberPages());
-    documentResponse.setYearPublication(document.getYearPublication());
-    documentResponse.setCode(document.getDocumentCode());
-    return documentResponse;
-  }
 }
---
+++ src/main/java/com/pe/relari/business/sqlite/expose/web/mapper/DocumentMapper.java
@@ -0,0 +1,21 @@
+package com.pe.relari.business.sqlite.expose.web.mapper;
+
+import com.pe.relari.business.sqlite.documents.model.api.CodeResponse;
+import com.pe.relari.business.sqlite.documents.model.api.DocumentRequest;
+import com.pe.relari.business.sqlite.documents.model.api.DocumentResponse;
+import com.pe.relari.business.sqlite.documents.model.domain.Document;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface DocumentMapper {
+
+  @Mapping(target = "id", ignore = true)
+  Document mapDocument(DocumentRequest documentRequest);
+
+  DocumentResponse mapDocumentResponse(Document document);
+
+  @Mapping(target = "documentCode", source = "code")
+  CodeResponse mapCodeResponse(String code);
+
+}
